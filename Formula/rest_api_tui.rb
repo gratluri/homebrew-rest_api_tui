b1789349c31a597abceeee44d465a9af3ccf1672
gratluri <31191009+gratluri@users.noreply.github.com>
2026-02-09 00:47:17 -0600

    Update URL for RestApiTui to new source

--- Formula/rest_api_tui.rb
+++ Formula/rest_api_tui.rb
@@ -1,7 +1,7 @@
 class RestApiTui  < Formula
   desc "Commandline UI for REST API test facility"
   homepage "https://github.com/gratluri/rest-api-tui"
-  url "https://github.com/gratluri/rest-api-tui/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/user-attachments/files/25175637/rest_api_tui-mac.tar.gz"
   sha256 "193e5d3196c7b3648eac8c9d40f19e0fd30c72a4b6e64d89c8b221e72bbb5c1f"
   version "v0.1.0"
 
